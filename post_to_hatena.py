3c80df50d5ff4725895783aaf717320cee88aa00
kazuyuki0217 <thelastrockstars47@gmail.com>
2026-08-21 19:45:52 +0900

    Refactor Hatena blog post submission process

--- post_to_hatena.py
+++ post_to_hatena.py
@@ -1,15 +1,17 @@
 import os
-import base64
-import hashlib
-import secrets
-from datetime import datetime, timezone
-from xml.sax.saxutils import escape
-
 import requests
+from xml.sax.saxutils import escape
 
 HATENA_ID = os.environ["HATENA_ID"]
 HATENA_API_KEY = os.environ["HATENA_API_KEY"]
-HATENA_ENDPOINT = os.environ["HATENA_ENDPOINT"].rstrip("/")
+
+# はてなブログAtomPubの投稿エンドポイント
+ENDPOINT = (
+    "https://blog.hatena.ne.jp/"
+    f"{HATENA_ID}/"
+    "tansinfuninkazu.hatenablog.com/"
+    "atom/entry"
+)
 
 title = os.environ.get("POST_TITLE", "自動投稿テスト")
 body = os.environ.get(
@@ -17,83 +19,25 @@ body = os.environ.get(
     "GitHub Actionsからの自動投稿テストです。"
 )
 
-# WSSE認証
-nonce = secrets.token_bytes(16)
-created = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-digest = hashlib.sha1(
-    nonce + created.encode("utf-8") + HATENA_API_KEY.encode("utf-8")
-).digest()
-
-password_digest = base64.b64encode(digest).decode("ascii")
-nonce_b64 = base64.b64encode(nonce).decode("ascii")
-
-wsse = (
-    'UsernameToken '
-    f'Username="{HATENA_ID}", '
-    f'PasswordDigest="{password_digest}", '
-    f'Nonce="{nonce_b64}", '
-    f'Created="{created}"'
-)
-
-headers = {
-    "X-WSSE": wsse,
-    "Accept": "application/atom+xml, application/xml, text/xml, */*",
-}
-
-# まずAtomPubルートから投稿先URLを取得
-response = requests.get(
-    HATENA_ENDPOINT,
-    headers=headers,
-    timeout=30,
-)
-
-print("ルート取得HTTPステータス:", response.status_code)
-print(response.text[:2000])
-
-response.raise_for_status()
-
-# AtomPubのPostURIを取得
-from xml.etree import ElementTree as ET
-
-root = ET.fromstring(response.content)
-
-post_uri = None
-
-for link in root.iter():
-    if link.tag.endswith("link"):
-        rel = link.attrib.get("rel", "")
-        href = link.attrib.get("href", "")
-        if rel == "service.post":
-            post_uri = href
-            break
-
-if not post_uri:
-    raise RuntimeError("AtomPubの投稿先(service.post)を取得できませんでした")
-
-print("投稿先:", post_uri)
-
-# 記事XML
 xml = f"""<?xml version="1.0" encoding="utf-8"?>
 <entry xmlns="http://purl.org/atom/ns#">
 <title>{escape(title)}</title>
 <content type="text/plain">{escape(body)}</content>
 </entry>"""
 
-# 投稿
 response = requests.post(
-    post_uri,
-    headers={
-        "X-WSSE": wsse,
-        "Content-Type": "application/atom+xml; charset=utf-8",
-        "Accept": "application/atom+xml, application/xml, text/xml, */*",
-    },
+    ENDPOINT,
+    auth=(HATENA_ID, HATENA_API_KEY),
     data=xml.encode("utf-8"),
+    headers={
+        "Content-Type": "application/atom+xml; charset=utf-8"
+    },
     timeout=30,
 )
 
-print("投稿HTTPステータス:", response.status_code)
-print(response.text[:3000])
+print("投稿先:", ENDPOINT)
+print("HTTPステータス:", response.status_code)
+print(response.text)
 
 response.raise_for_status()
 
